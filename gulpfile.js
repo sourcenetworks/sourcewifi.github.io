cca7af2d6a4a936c1c2599a548a6efda79208ebd
Ian Sibner <sibnerian@gmail.com>
2017-01-18 23:27:27 -0800

    Update 2017-01-19T07:27:25.358Z

---
+++ gulpfile.js
@@ -0,0 +1,52 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var autoprefixer = require('gulp-autoprefixer');
+var minify = require('gulp-minify-css');
+var plumber = require('gulp-plumber');
+var livereload = require('gulp-livereload');
+var connect = require('gulp-connect');
+var opener = require('opener');
+var deploy = require('gulp-gh-pages');
+
+function onError(err) {
+  console.error(err);
+}
+
+gulp.task('sass', function(){
+  return gulp.src('styles.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(autoprefixer({
+      browsers: ['last 2 versions', 'Explorer >= 9', 'Android >= 4.1', 'Safari >= 7', 'iOS >= 7']
+    }))
+    .pipe(minify())
+    .pipe(gulp.dest('css/'))
+    .pipe(plumber({
+        errorHandler: onError
+    }))
+    .pipe(livereload());
+});
+
+gulp.task('connect', function() {
+  connect.server({
+    livereload: true
+  });
+});
+
+gulp.task('watch', function() {
+  livereload.listen();
+  gulp.watch('**/*.scss', ['sass']);
+  gulp.watch('index.html', function (changeEvent) {
+    livereload.changed(changeEvent.path);
+  });
+});
+
+gulp.task('open', function(){
+  opener('http://localhost:8080');
+});
+
+gulp.task('default', ['sass', 'watch', 'connect', 'open']);
+
+gulp.task('deploy', ['sass'], function () {
+  return gulp.src(['./**/*', '!node_modules/**/*'])
+    .pipe(deploy());
+});
