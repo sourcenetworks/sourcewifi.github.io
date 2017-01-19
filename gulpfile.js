6881fb7901600292db4f27e12f0eb586c94cb315
Dillon Chen <dillonchen13@gmail.com>
2017-01-19 15:08:55 -0500

    Update 2017-01-19T20:08:49.695Z

--- gulpfile.js
+++ gulpfile.js
@@ -19,7 +19,7 @@ gulp.task('sass', function(){
       browsers: ['last 2 versions', 'Explorer >= 9', 'Android >= 4.1', 'Safari >= 7', 'iOS >= 7']
     }))
     .pipe(minify())
-    .pipe(gulp.dest('css/'))
+    .pipe(gulp.dest('css/build/'))
     .pipe(plumber({
         errorHandler: onError
     }))
@@ -51,4 +51,9 @@ gulp.task('open', function(){
   opener('http://localhost:8080');
 });
 
-gulp.task('default', ['sass', 'watch', 'connect', 'open', 'deploy']);
+gulp.task('default', ['sass', 'watch', 'connect', 'open']);
+
+gulp.task('deploy', ['sass'], function () {
+  return gulp.src(['./**/*', '!node_modules/**/*'])
+    .pipe(deploy());
+});
