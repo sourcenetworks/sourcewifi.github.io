6b2a82a1a318912988fda64a08c5f5676cce9bd7
Joe Farned <joefarned@gmail.com>
2017-02-28 00:02:21 -0500

    script update

--- install.sh
+++ install.sh
@@ -2,57 +2,75 @@
 
 # This is the source install script
 #
-# MAC AND LINUX:
-#   Just open up your terminal and type:
-#
-#     curl https://sourcewifi.com | sh
+# Usage: curl sourcewifi.com/install.sh | sudo sh
 #
 #   Currently supports:
-#    - Linux: x86 and x86_64 systems
+#     Raspbian 4.4
 #
-###########
 
-# TODO: run ping and if not output, then output error message
-# wget -q --tries=10 --timeout=5 --spider http://google.com
-# if [[ $? -eq 0 ]]; then
-#  echo "Online"
-# else
-#  echo "Offline"
-#  exit
-# fi
+# TODO: update for versioning
+install_requirements () {
+  sudo apt-get -y update
 
-# Install node, hostapd, dnsmasq
-sudo apt-get -y update
-sudo curl -sL https://deb.nodesource.com/setup_7.x | sudo -E bash -
-sudo apt-get -y install nodejs
-sudo apt-get -y install npm
-sudo apt-get -y install hostapd
-sudo apt-get -y install dnsmasq
+  PGK_OK=$(dpkg -s nodejs | grep "install ok installed")
+  if [ "" = "$PKG_OK" ]; then
+    sudo curl -sL https://deb.nodesource.com/setup_7.x | sudo -E bash -
+    sudo apt-get -y install nodejs
+  fi
 
-# TODO: this should not require password - need public release
-git clone https://github.com/sourcewifi/source-router
-sudo cp source-router/source-config/dnsmasq.conf /etc/dnsmasq.conf
-sudo cp source-router/source-config/hosts /etc/hosts
-sudo cp source-router/source-config/hostapd.conf /etc/hostapd/hostapd.conf
-sudo cp source-router/source-config/interfaces /etc/network/interfaces
+  PGK_OK=$(dpkg -s npm | grep "install ok installed")
+  if [ "" = "$PKG_OK" ]; then
+    sudo curl -sL https://deb.nodesource.com/setup_7.x | sudo -E bash -
+    sudo apt-get -y install npm
+  fi
 
-# Project dependencies; run a subprocesses
-sudo npm --prefix ./source-router/source-firewall install
-(cd source-router/source-firewall; sudo npm link)
-(cd source-router/source-server; sudo npm link source-firewall)
-sudo npm --prefix ./source-router/source-server install
+  PGK_OK=$(dpkg -s hostpad | grep "install ok installed")
+  if [ "" = "$PKG_OK" ]; then
+    sudo apt-get -y install hostapd
+  fi
 
-# IPv4 packet forwarding
-sudo sed -i '/#net.ipv4.ip_forward=1/c\net.ipv4.ip_forward=1' /etc/sysctl.conf
+  PGK_OK=$(dpkg -s dnsmasq | grep "install ok installed")
+  if [ "" = "$PKG_OK" ]; then
+    sudo apt-get -y install dnsmasq
+  fi
+}
 
-# hostpad daemon
-sudo sed -i '/#DAEMON_CONF=""/c\DAEMON_CONF="/etc/hostapd/hostapd.conf"' /etc/default/hostapd
+install_source () {
+  git clone https://github.com/sourcenetworks/source
+  sudo cp source/conf/dnsmasq.conf /etc/dnsmasq.conf
+  sudo cp source/conf/hosts /etc/hosts
+  sudo cp source/conf/hostapd.conf /etc/hostapd/hostapd.conf
+  sudo cp source/conf/interfaces /etc/network/interfaces
 
-# Bring up hostapd, dnsmasq
-sudo service dnsmasq start
-sudo service hostapd start
-sudo ifconfig wlan0 192.168.24.1
-sudo service dnsmasq restart
-sudo service hostapd restart
+  sudo npm --prefix ./source/lib/source-firewall install
+  (cd source/lib/source-firewall; sudo npm link)
+  (cd source/server; sudo npm link source-firewall)
+  sudo npm --prefix ./source/server install
 
-sudo npm --prefix ./source-router/source-server start
+  # IPv4 packet forwarding
+  sudo sed -i '/#net.ipv4.ip_forward=1/c\net.ipv4.ip_forward=1' /etc/sysctl.conf
+
+  # hostpad daemon
+  sudo sed -i '/#DAEMON_CONF=""/c\DAEMON_CONF="/etc/hostapd/hostapd.conf"' /etc/default/hostapd
+}
+
+start_services () {
+  sudo service dnsmasq start
+  sudo service hostapd start
+  sudo ifconfig wlan0 192.168.24.1
+  sudo service dnsmasq restart
+  sudo service hostapd restart
+}
+
+main () {
+  # TODO: fix
+  PGK_OK=$(dpkg -s hostapd | grep "install ok installed")
+  if [ "" = "$PKG_OK" ]; then
+    install_requirements
+  fi
+
+  install_source
+  start_services
+}
+
+main
